9579d582de23d9ca77914f40b05aafabfc29d0cb
Deadly <info.saddiki@gmail.com>
2022-07-09 14:15:20 +0200

    Added different code structure, prepping for logging

--- server.py
+++ server.py
@@ -1,52 +1,110 @@
+import logging
 import socket
-from sys import argv
-
 import threading
-from random import randint
+from sys import argv
 from typing import Union
-from datetime import datetime
-
-from aenum import enum
 
 
-class Actions(enum):
-    connect = 1
-    verify = 2
-    ping = 3
-    fetch = 4
+def PerformLogging(logger: Union[logging.Logger, None], level: int, message: Union[str, None]):
+    def wrap(f):
+        def wrapped_f(*args, **kwargs):
+            if logger is None:
+                server_logger.log(level, message or "no message")
+            f(*args, **kwargs)
+
+        return wrapped_f
+
+    return wrap
+
 
 class Client:
     def __init__(self, name: Union[str, None], address: tuple, connection: socket.socket):
-        self.name: str = name
+        self.name: Union[str, None] = name
         self.address: tuple = address,
-        self.last_action: int = Actions.connect
-        self.last_action_time: datetime = datetime.now()
         self.connection: socket.socket = connection
+        self.logger: Union[logging.Logger, None] = None
+        self.thread: Union[threading.Thread, None] = None
 
+    @PerformLogging(None, logging.DEBUG, "Sending message")
+    def send(self, message: str):
+        self.connection.sendall(message.__add__("\n").encode("utf-8"))
 
-    def close_connection(self):
+    @PerformLogging(None, logging.DEBUG, "Sending message, then disconnecting")
+    def send_then_disconnect(self, message: str):
+        self.connection.sendall(message.__add__("\n").encode("utf-8"))
+        self.close_connection()
+
+    @PerformLogging(None, logging.DEBUG, "Sending message, then disconnecting")
+    def verify(self, name: str):
+        if name.__len__() > 25:
+            self.send_then_disconnect("Error:?")
+        self.name = name.split(":")[1].removesuffix("\n")
+        self.logger = logging.getLogger(f"Clients.{self.name}")
+
+    def close_connection(self, reason: Union[str, None] = None):
+        if self.logger is None:
+            clients_logger.info(f"Manually disconnecting {self.address[0]} for: {reason or 'no reason specified'}")
+        self.logger.warning(f"Manually disconnecting {self.name} for: {reason or 'no reason specified'}")
         if self in client_list:
             client_list.remove(self)
         self.connection.close()
 
-    def
+    def register_thread(self, thread_address: threading.Thread):
+        self.thread = thread_address
 
 
-def log(message: str, client: Union[Client, None]):
-    now = datetime.now().strftime('%H:%M:%S')
-    message = message.removesuffix("\n")
+class Utilities:
+    @staticmethod
+    def get_all_user_names():
+        result = list()
 
-    if client is None:
-        print(f"[{now}] - {message}")
-        return
-    print(f"[{now}] ({client.address[0]}) - {message}")
-    return
+        for client in client_list:
+            if client.name is None:
+                continue
+            result.append(client.name)
+        return result or None
+
+    @staticmethod
+    def get_all_user_names_raw():
+        result = str()
+
+        for client in client_list:
+            if client.name is None:
+                continue
+            if result == str():
+                result.__add__(client.name)
+            else:
+                result.__add__(f",{client.name}")
+        return result or None
+
+    @staticmethod
+    def get_user_by_name(name: str):
+        for client in client_list:
+            if client.name == name:
+                return client
+        return None
+
+    @staticmethod
+    def get_user_by_address(address: str):
+        for client in client_list:
+            if client.address == address:
+                return client
+        return None
+
+    @staticmethod
+    def handle_argument(message: str, delimiter: str = ":"):
+        return message.split(delimiter)[1].removesuffix("\n")
+
+    @staticmethod
+    def strip_argument(message: str, delimiter: str = ":"):
+        return message.split(delimiter)[0].removesuffix("\n")
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-last_run = None
+server_logger = logging.getLogger("Server")
+clients_logger = logging.getLogger("Clients")
 client_list = list()
 
 try:
@@ -56,111 +114,76 @@ except IndexError:
     ip_address = socket.gethostbyname(socket.gethostname())
     port = 5001
 
-
-log("Booting up server", None)
+server_logger.log(logging.CRITICAL, f"Booting up server on {ip_address}:{port}")
 server.bind((ip_address, port))
 server.listen(5)
-
-
-def get_all_user_names():
-    result = []
-
-    for client in client_list:
-        if client.name is None:
-            result.append(f"Unverified-{randint(0,999)}")
-            continue
-        result.append(client.name)
-    return result or None
-
-
-def get_user_by_name(name: str):
-    for client in client_list:
-        if client.name == name:
-            return client
-    return None
-
-
-def get_user_by_address(address: str):
-    for client in client_list:
-        if client.address == address:
-            return client
-    return None
 
 
 def client_thread(client: Client):
     connection = client.connection
 
-    log("New client connected", client)
-    connection.send("Connected:True\n".encode())
+    client.send("Connected:?")
 
     while True:
         try:
-            message = connection.recv(2048).decode("utf-8")
+            message = connection.recv(2048).decode("utf-8") \
+                .removeprefix("\n") \
+                .removesuffix("\n")
 
             if message:
                 if client.name is None:
                     if "Name:" not in message:
-                        log(f"Kicking user for bad verification: {message}", client)
-                        connection.sendall("Verified:?\n".encode())
-                        remove(client)
+                        client.logger.error(f"Kicking user for bad verification")
+                        client.send_then_disconnect("Error:?")
                         return
-                    else:
-                        client.name = message.split(":")[1].removesuffix("\n")
-                        log(f"Verified user as: {client.name}", client)
-                        connection.sendall(f"Verified:{client.name}\n".encode())
+                    client.name = Utilities.handle_argument(message)
+                    client.logger = logging.getLogger(f"Clients.{client.name}")
+                    client.logger.info(f"Verified user as: {client.name}")
+                    client.send(f"Verified:{client.name}")
 
-                elif message in ["FetchUsers:?", "FetchUsers:?\n"]:
-                    result = ""
-                    for i in get_all_user_names():
-                        if result == "":
-                            result += i
-                            continue
-                        result += "," + i
-                    log(f"Fetched all users: {result}", client)
-                    connection.sendall(f"Users:{result}\n".encode())
+                match Utilities.strip_argument(message):
+                    case "Ping":
+                        username = Utilities.handle_argument(message)
+                        target = Utilities.get_user_by_name(username)
 
-                elif "Ping:" in message:
-                    raw_message = message.split(":")[1].removesuffix("\n")
-                    target = get_user_by_name(raw_message)
+                        if target is None:
+                            connection.sendall("Error:?\n".encode())
+                        else:
+                            client.logger.info(f"Sent ping to {target.name} from {client.name}")
+                            client.send(f"SentPing:{target.name}")
+                            target.send(f"ReceivedPing:{client.name}")
+                    case "Fetch":
+                        result = Utilities.get_all_user_names_raw()
+                        client.logger.info(f"Fetched all users: {result}")
+                        client.send(f"Users:{result}")
+                    case "Exit":
+                        client.send_then_disconnect("Bye:?")
+                        return
+                    case "Name":
+                        pass
+                    case _:
+                        client.logger.warning(f"Unknown command: {message}")
+                        connection.sendall("Error:?\n".encode())
 
-                    if target is None:
-                        connection.sendall("UserNotFound:?\n".encode())
-                    else:
-                        log(f"Sent ping to {target.name} from {client.name}", client)
-                        connection.sendall(f"SentPing:{target.name}\n".encode())
-                        target.connection.sendall(f"ReceivedPing:{client.name}\n".encode())
-
-                elif "Exit:" in message:
-                    connection.sendall("Bye:?\n".encode())
-                    remove(client)
-                    return
-
-                else:
-                    log(f"Unknown command: {message}", client)
-                    connection.sendall("CommandNotFound:?\n".encode())
             else:
-                log(f"Gracefully disconnecting {client.name}", client)
-                remove(client)
+                client.logger.warning(f"Gracefully disconnecting {client.name}")
+                client.close_connection()
                 return
         except Exception as e:
-            log(f"Force disconnecting {client.name}, due to: {e.__str__()}", client)
-            remove(client)
+            client.logger.error(f"Force disconnecting {client.name}, due to: {e.__str__()}")
+            client.close_connection()
             return
-
-
-def remove(client: Client):
-    if client in client_list:
-        client_list.remove(client)
-    client.connection.close()
 
 
 while True:
     conn, addr = server.accept()
 
-    new_user = Client(addr, None, conn)
-    client_list.append(new_user)
+    user = Client(None, addr, conn)
+    client_list.append(user)
 
-    threading.Thread(target=client_thread, args=(new_user,)).start()
+    thread = threading.Thread(target=client_thread, args=(user,), name=f"Client: {addr[0]}")
+    user.register_thread(thread)
+    user.thread.start()
 
 conn.close()
 server.close()
