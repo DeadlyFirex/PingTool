162e8cfa507d11bc814de100c0117d0997fbb80c
Deadly <info.saddiki@gmail.com>
2024-01-01 23:25:17 +0100

    Updated client implementation

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 import socket
 import loguru
+import argparse
 
 from playsound import playsound
 from select import select
-from sys import argv, stdin, stdout
+from secrets import token_hex
+from sys import stdin, stdout
 from typing import Union
-from assets.__init__ import *
+from json import loads
 
 
 class Connection:
-    def __init__(self, name: Union[str, None], address: tuple, connection: socket.socket):
-        self.name: Union[str, None] = name
+    def __init__(self, name: str | None, address: tuple, connection: socket.socket):
+        self.name: str | None = name
         self.address: tuple = address
         self.connection: socket.socket = connection
 
         connection.connect(address)
+        self.config: dict | None = loads(self.connection.recv(2048)
+                                         .decode("utf-8")
+                                         .removeprefix("\n")
+                                         .removesuffix("\n")
+                                         .removeprefix("Connected:"))
 
     def send(self, message: str):
         self.connection.sendall(message.__add__("\n").encode("utf-8"))
@@ -24,17 +31,22 @@ class Connection:
         self.disconnect()
 
     def verify(self, name: str):
-        if name.__len__() > 25:
-            raise ValueError("Name too long")
+        min_len, max_len = (self.config.get("settings").get("username_min_length"),
+                            self.config.get("settings").get("username_max_length"))
+
+        if name is None or name == "":
+            raise ValueError("Name cannot be empty")
+        if min_len > name.__len__() > max_len:
+            raise ValueError("Name length is invalid")
         self.name = name
         self.send(f"Name:{name}")
+
+    def get_config(self):
+        pass
 
     def disconnect(self, reason: Union[str, None] = None):
         loguru.logger.warning(f"Manually disconnecting {self.name} for: {reason or 'no reason specified'}")
         self.connection.close()
-
-    def log(self, message: Union[str, None], level: Union[str, int] = DEBUG.name):
-        loguru.logger.log(level, message)
 
     @staticmethod
     def handle_argument(message: str, delimiter: str = ":"):
@@ -45,30 +57,35 @@ class Connection:
         return message.split(delimiter)[0].removesuffix("\n")
 
 
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-file_logger = loguru.logger.add(
+# Setup logger
+app_logger = loguru.logger.add(
     sink="logs/client.log",
     level="TRACE",
     rotation="15MB",
     compression="zip"
 )
 
+# Setup command line arguments
+parser = argparse.ArgumentParser(description="Application to demonstrate PingTool servers")
+parser.add_argument('--host', type=str, help='The host to bind the server to')
+parser.add_argument('--port', type=int, help='The port number to bind the server to')
+parser.add_argument('--name', type=str, help='The name to connect with')
+args = parser.parse_args()
+
+# Setup connection
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+host = args.host if args.host else socket.gethostbyname(socket.gethostname())
+port = args.port if args.port else 12500
+name = args.name if args.name else token_hex(6)
 input_list = [stdin, conn]
 
-try:
-    HOST = str(argv[1])
-    PORT = int(argv[2])
-except IndexError:
-    HOST = socket.gethostbyname(socket.gethostname())
-    PORT = 5001
-
-loguru.logger.info(f"Booting client, setting to connect to {HOST}:{PORT}")
+loguru.logger.info(f"Booting client, setting to connect to {host}:{port}")
 
 try:
-    connection = Connection(None, (HOST, PORT), conn)
-    connection.verify("Test")
+    connection = Connection(None, (host, port), conn)
+    connection.verify(name)
 
     data = conn.recv(10024).decode('utf-8').removeprefix('\n').removesuffix('\n')
 
@@ -94,3 +111,6 @@ try:
 except KeyboardInterrupt:
     connection.disconnect()
     exit(0)
+except ConnectionRefusedError:
+    loguru.logger.error(f"Failed to connect to {host}:{port}")
+    exit(1)
