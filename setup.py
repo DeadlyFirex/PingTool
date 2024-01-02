20df25971ea2dc624dcdb47f8da4e4ac40932c2c
Deadly <info.saddiki@gmail.com>
2024-01-02 22:15:53 +0100

    Changed setup to fix build failure

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as req:
-    requirements = [i.strip() for i in req.readlines()]
-
 setup(
     name='PingTool',
-    version='1.0.0',
+    version='v1.1.5',
     url='https://github.com/DeadlyFirex/PingTool',
     license='MIT',
+    license_file='LICENSE',
     author='deadly',
     author_email='',
     description='Small application written in Python, socket-based. to ping annoying colleague\'s who won\'t respond.',
@@ -19,15 +17,19 @@ setup(
     project_urls={
         "Bug Tracker": "https://github.com/DeadlyFirex/PingTool/issues",
     },
-
     packages=find_packages(),
+    platforms="any",
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: GNU AGPLv3 License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
+    install_requires=[
+        "loguru>=0.7.0",
+        "setuptools>=69.0.3"
+    ],
     python_requires=">=3.10",
+    setup_requires=["setuptools>=69.0.0"],
 )
