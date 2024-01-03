e9239d812e11e5e6fe46c3f08fefc4aadaed3fb7
Deadly <info.saddiki@gmail.com>
2024-01-03 07:26:37 +0100

    Updated versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='PingTool',
-    version='v1.1.5',
+    version='v1.2.0',
     url='https://github.com/DeadlyFirex/PingTool',
     license='MIT',
     license_file='LICENSE',
