7a949bcef191708293c553914822db9e368fc751
Deadly <info.saddiki@gmail.com>
2024-01-03 07:18:25 +0100

    Client model split up into seperate file

--- models/client.py
+++ models/client.py
@@ -1,45 +1,73 @@
 import loguru
 
+from utils.__init__ import DEBUG, TRACE
+from utils.helpers import perform_logging
+
 from socket import socket
 from threading import Thread
-
-from assets.__init__ import *
-from utils.helpers import perform_logging, client_list
+from typing import Union
 
 
 class Client:
-    @perform_logging(TRACE.name, "Creating user")
-    def __init__(self, name: str | None, address: tuple, connection: socket):
-        self.name: str | None = name
+    """
+    Class represents a client connection.
+    """
+    @perform_logging(TRACE.name, "Creating new client")
+    def __init__(self, name: Union[str, None], address: tuple, connection: socket) -> None:
+        """
+        Initialize a new client connection.
+        :param name: Name of client
+        :param address: IP Address of client
+        :param connection: Socket connection
+        """
+        self.name: Union[str, None] = name
         self.address: tuple = address
         self.connection: socket = connection
-        self.thread: Thread | None = None
+        self.thread: Union[Thread, None] = None
 
-    @perform_logging(TRACE.name, "Sending message")
-    def send(self, message: str):
+    @perform_logging(TRACE.name, "Sending message to client")
+    def send(self, message: str) -> None:
+        """
+        Send a message to the client.
+        :param message: Message to send
+        :return: null
+        """
         self.connection.sendall(message.__add__("\n").encode("utf-8"))
 
-    @perform_logging(TRACE.name, "Sending message, then disconnecting")
-    def send_then_disconnect(self, message: str):
+    @perform_logging(TRACE.name, "Sending message to client, then disconnecting")
+    def send_then_disconnect(self, message: str) -> None:
+        """
+        Send a message to the client, then disconnect.
+        :param message: Message to send
+        :return: null
+        """
         self.connection.sendall(message.__add__("\n").encode("utf-8"))
         self.close_connection()
 
-    @perform_logging(TRACE.name, "Verifying user")
-    def verify(self, name: str):
-        self.name = name.split(":")[1].removesuffix("\n")
-
     @perform_logging(TRACE.name, "Disconnecting user")
-    def close_connection(self, reason: str | None = None):
-        loguru.logger.warning(f"Manually disconnecting {self.name or 'undefined'} for:"
-                              f" {reason or 'no reason specified'}")
-        if self in client_list:
-            client_list.remove(self)
+    def close_connection(self) -> None:
+        """
+        Close the connection with the client.
+        :return: null
+        """
         self.connection.close()
+        return
 
     @perform_logging(TRACE.name, "Registering thread to client")
-    def register_thread(self, thread_address: Thread):
+    def register_thread(self, thread_address: Thread) -> None:
+        """
+        Register the thread to the client.
+        :param thread_address: Thread to register
+        :return: null
+        """
         self.thread = thread_address
 
     @perform_logging(TRACE.name, "Logging message")
-    def log(self, message: str | None, level: str | int = DEBUG.name):
+    def log(self, message: Union[str, None], level: Union[str, int] = DEBUG.name) -> None:
+        """
+        Log a message.
+        :param message: Message to log
+        :param level: Level of logging
+        :return: null
+        """
         loguru.logger.log(level, message)
