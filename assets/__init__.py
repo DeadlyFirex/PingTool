51c358eacecd7695c4c8c18f23343e52feca8791
Deadly <info.saddiki@gmail.com>
2024-01-03 07:11:08 +0100

    Removed assets package and merged into utils

--- assets/__init__.py
+++
@@ -1,9 +0,0 @@
-from loguru import logger
-
-TRACE = logger.level("TRACE")
-DEBUG = logger.level("DEBUG")
-INFO = logger.level("INFO")
-SUCCESS = logger.level("SUCCESS")
-WARNING = logger.level("WARNING")
-ERROR = logger.level("ERROR")
-CRITICAL = logger.level("CRITICAL")
