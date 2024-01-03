4d8c4564f9e748570640b08d90e2b077192b6e5a
Deadly <info.saddiki@gmail.com>
2024-01-03 07:20:41 +0100

    Replaced logging levels

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+import loguru
+
+# Logger levels
+TRACE = loguru.logger.level("TRACE")
+DEBUG = loguru.logger.level("DEBUG")
+INFO = loguru.logger.level("INFO")
+SUCCESS = loguru.logger.level("SUCCESS")
+WARNING = loguru.logger.level("WARNING")
+ERROR = loguru.logger.level("ERROR")
+CRITICAL = loguru.logger.level("CRITICAL")
